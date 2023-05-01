d68c3735f06cbd71a639e2a19fe2026846eabf1f
Marko Lainovic <marko.lainovic@gmail.com>
2023-05-01 21:39:05 +0200

    add CORS

--- src/pages/api/context.js
+++ src/pages/api/context.js
@@ -1,30 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { v4 as uuid } from "uuid";
 import { decode } from 'base64-arraybuffer'
+import Cors from 'cors'
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
 async function handler(req, res) {
-  const { imgUrl, context } = req.body
-  console.log({ imgUrl });
-  console.log({ context });
-
-  const imgRes = await fetch(imgUrl)
-  const buffer = await imgRes.arrayBuffer();
-  const b64 = Buffer.from(buffer).toString("base64");
-
+  await runMiddleware(req, res, cors);
+  const { b64Image, imageType } = req.body
   const { data, error } = await supabase
     .storage
     .from('images')
-    .upload(`${uuid()}`, decode(b64),
-      { contentType: imgRes.headers.get("content-type") })
+    .upload(`${uuid()}`, decode(b64Image),
+      { contentType: imageType })
   if (error) {
     res.status(400).json({ error });
   } else {
-    res.status(200).json({ data });
+    const savedImageUrl = data.path;
+    console.log({ savedImageUrl });
+    res.status(200).json({ savedImageUrl });
   }
 };
 
